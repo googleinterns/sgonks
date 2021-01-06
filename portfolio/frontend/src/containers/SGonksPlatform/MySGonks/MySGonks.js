d81023ec5c6f443d56a119a2f172531ef80dba56
lin8231 <lin8231@outlook.com>
2021-01-07 10:02:44 +1300

    Set up layout of info section of protoype of MySgonks page

--- portfolio/frontend/src/containers/SGonksPlatform/MySGonks/MySGonks.js
+++ portfolio/frontend/src/containers/SGonksPlatform/MySGonks/MySGonks.js
@@ -2,26 +2,35 @@ import React from "react"
 import classes from "./MySGonks.module.css"
 import placeholderChart from "../../../assets/placeholderChart.png"
 import Marketplace from "../Marketplace/Marketplace"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
 const MySGonks = (props) => {
   return (
     <div className={classes.MySGonks}>
       <div className={classes.ChartAndInfoContainer}>
         <div className={classes.ChartContainer}>
-          <img src={placeholderChart} className={classes.PlaceholderChart}></img>
+          <img
+            src={placeholderChart}
+            className={classes.PlaceholderChart}
+          ></img>
         </div>
         <div className={classes.InfoContainer}>
           <div className={classes.NameInfoContainer}>
-            <p><strong>Firstname Lastname</strong></p>
+            <p>
+              <strong>Firstname Lastname</strong>
+            </p>
             <p>Initial Worth: t$100</p>
             <p>Net worth: t$19492</p>
           </div>
-          <div className={classes.currentlyAvailableContainer}>
-            <p><strong>Currently available:</strong></p>
+          <div className={classes.CurrentlyAvailableContainer}>
+            <p>
+              <strong>Currently available:</strong>
+            </p>
             <p>t$4918</p>
           </div>
-          <Link to="/marketplace" >Buy sGonks</Link>
+          <div className={classes.BuyButton}>
+            <Link to="/marketplace">Buy sGonks</Link>
+          </div>
         </div>
       </div>
       <div className={classes.SGonksTable}>
