ff5b8aab0558fa8aaec320f1584b9b6ea600483f
roonil-wazlib <emma.hogan314@gmail.com>
2021-01-06 16:41:48 +1300

    Add janky dashboard view

--- portfolio/frontend/src/containers/SGonksPlatform/Dashboard/Dashboard.js
+++ portfolio/frontend/src/containers/SGonksPlatform/Dashboard/Dashboard.js
@@ -3,8 +3,63 @@ import classes from "./Dashboard.module.css"
 
 const Dashboard = (props) => {
   return (
-    <div>
-      <p>This is the Dashboard Page.</p>
+    <div className={classes.Body}>
+      <div className={classes.Column}>
+        <p className={classes.BigFont}>Welcome back, Firstname!</p>
+        <div className={classes.Box}>
+          <p>Time until end of competition:</p>
+          <p className={classes.MediumFont}>20 Days 19:54:10</p>
+          <p>Your current ranking:</p>
+          <p className={classes.BigFont}>2nd</p>
+          <p>+1 from yesterday</p>
+        </div>
+
+        <div className={classes.Box}>
+          <p>Emma Hogan invested</p>
+          <p className={classes.MediumFont}>t$429180</p>
+          <p className={classes.MediumFont}>in "bitcoin"</p>
+          <p>17th December 2020 at 21:00 UTC</p>
+        </div>
+      </div>
+
+      <div className={classes.Column}>
+        <div className={classes.Box}>
+          <p>Your sGonks</p>
+          <ul>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+          </ul>
+        </div>
+        <div className={classes.Box}>
+          <img src='chart_placeholder.jpg' alt="placeholder chart"/>
+        </div>
+        <button>View my sGonks</button>
+      </div>
+
+      <div className={classes.Column}>
+        <div className={classes.Box}>
+          <p>Currently available:</p>
+          <p className={classes.MediumFont}>t$4918</p>
+          <p>New worth:</p>
+          <p className={classes.MediumFont}>t$19492</p>
+        </div>
+        <button>Buy sGonks</button>
+        <div className={classes.Box}>
+          <p className={classes.MediumFont}>Trending investments:</p>
+          <ul>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+            <li>chicken wings</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
