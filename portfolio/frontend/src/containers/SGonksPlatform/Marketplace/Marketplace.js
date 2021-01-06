233ee74399bec88ef7a62e0778dc6f5f5db559ce
Phoebe Khokgawe <khokgawep@gmail.com>
2021-01-07 11:16:29 +1300

    Finish adding hardcoded data on the market page

--- portfolio/frontend/src/containers/SGonksPlatform/Marketplace/Marketplace.js
+++ portfolio/frontend/src/containers/SGonksPlatform/Marketplace/Marketplace.js
@@ -7,38 +7,47 @@ const Marketplace = (props) => {
     <div className={classes.MarketPlace}>
       <div className={classes.GoogleSearchDisplayedConatainer}>
         <div className={classes.GoogleSearchContainer}>
-        <p>Google Search here</p>
-        <br></br>
-        <input type="text" id="Google-Search" name="Google-Search"></input>
-        <br></br>
-        <br></br>
-        <br></br>
-        <img src={placeholderMarket}></img>
+          <div className={classes.BigText}>
+            <p>Google Search here</p>
+          </div>
+          <br></br>
+          <input type="text" id="Google-Search" name="Google-Search"></input>
+          <br></br>
+          <br></br>
+          <br></br>
+        </div>
+        <div className={classes.ChartContainer}>
+          <img src={placeholderMarket}></img>
         </div>
         <div className={classes.GoogleSearchInfoContainer}>
           <div className={classes.BigText}>
             <p>"Chick Wings"</p>
           </div>
-          <p>Amout to purchase:       t$</p>
-          <p>Currently avaliable:     t$</p>
+          <p>Amout to purchase: t$</p>
+          <p>Currently avaliable: t$</p>
+          <div className={classes.button}>
+            <button class="button">Confirm Purchase</button>
+          </div>
         </div>
       </div>
       <div className={classes.RightColumnConatainer}>
+        <div className={classes.BigText}>
+          <p> Trending investements</p>
+        </div>
         <div className={classes.TrendingInvestmentContainer}>
-          <div className={classes.BigText}>
-            <p> Trending investements</p>
-          </div>
-          <p> 1.  new year 2021</p>
-          <p> 1.  new year 2021</p>
-          <p> 1.  new year 2021</p>
-          <p> 1.  new year 2021</p>
-          <p> 1.  new year 2021</p>
-          <p> 1.  new year 2021</p>
-          <p> 1.  new year 2021</p>
-          <p> 1.  new year 2021</p>
+          <p> 1. new year 2021</p>
+          <p> 1. new year 2021</p>
+          <p> 1. new year 2021</p>
+          <p> 1. new year 2021</p>
+          <p> 1. new year 2021</p>
+          <p> 1. new year 2021</p>
+          <p> 1. new year 2021</p>
+          <p> 1. new year 2021</p>
+        </div>
+        <div className={classes.BigText}>
+          <p>See what your team is buying:</p>
         </div>
         <div className={classes.TeammatesInvestmentContainer}>
-          <p>See what your team is buying:</p>
           <p>Emma Hogan</p>
           <p>invested</p>
           <div className={classes.BigText}>
@@ -55,13 +64,4 @@ const Marketplace = (props) => {
   )
 }
 
-
-{/* <div className={classes.TrendingInvestmentContainer}>
-        <div>1.   new year 2020</div>
-        <div>2.   new year 2020</div>
-        <div>3.   new year 2020</div>
-        <div>4.   new year 2020</div>
-        </div> */}
-
 export default Marketplace
-
