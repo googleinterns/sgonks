93e53bea3680972eef21049dfb7f9ce863e6ee04
Phoebe Khokgawe <khokgawep@gmail.com>
2021-01-06 18:43:08 +1300

    Add Hardcode data on the Market page

--- portfolio/frontend/src/containers/SGonksPlatform/Marketplace/Marketplace.js
+++ portfolio/frontend/src/containers/SGonksPlatform/Marketplace/Marketplace.js
@@ -1,12 +1,30 @@
 import React from "react"
 import classes from "./Marketplace.module.css"
+import placeholderChart from "../../../assets/placeholderChart.png"
 
 const Marketplace = (props) => {
   return (
-    <div>
-      <p>This is the Marketplace page.</p>
+    <div className={classes.MarketPlace}>
+      <div className={classes.GoogleSearchDisplayedConatainer}>
+        <div className={classes.GoogleSearchContainer}>
+        <p>Google Search here</p>
+        <input type="text" id="Google-Search" name="Google-Search"></input>
+        </div>
+        <img src={placeholderChart}></img>
+      </div>
+      <div className={classes.LeftConatainer}>
+      </div>
     </div>
   )
 }
 
+
+{/* <div className={classes.TrendingInvestmentContainer}>
+        <div>1.   new year 2020</div>
+        <div>2.   new year 2020</div>
+        <div>3.   new year 2020</div>
+        <div>4.   new year 2020</div>
+        </div> */}
+
 export default Marketplace
+
