2a3334f54e15a858fc5e2fca2df2a0dffb268fae
Phoebe Khokgawe <khokgawep@gmail.com>
2021-01-07 12:59:31 +1300

    Add new files for CreateCompetition page.

---
+++ portfolio/frontend/src/containers/CreateCompetition/CreateCompetition.js
@@ -0,0 +1,12 @@
+import React from "react"
+import classes from "./CreateCompetition.module.css"
+
+const CreateCompetition = (props) => {
+  return (
+    <div>
+      <p>This is the CreateCompetition Page.</p>
+    </div>
+  )
+}
+
+export default CreateCompetition
