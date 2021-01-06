3229e928a8c84b64d4867d7e3e62cf778a31ef83
lin8231 <lin8231@outlook.com>
2021-01-06 15:20:26 +1300

    Populate competition prototype with hard-coded html data

--- portfolio/frontend/src/containers/SGonksPlatform/Competition/Competition.js
+++ portfolio/frontend/src/containers/SGonksPlatform/Competition/Competition.js
@@ -5,12 +5,85 @@ const Competition = (props) => {
   return (
     <div className={classes.Competition}>
       <div className={classes.RankingInfoContainer}>
-        <div className={classes.RankingsContainer}></div>
-        <div className={classes.RankingChartContainer}></div>
+        <div className={classes.RankingsContainer}>
+          <div className={classes.RankingRows}>
+            <div className={classes.Row}>
+              <div>Number</div>
+              <div>sGonks Investor</div>
+              <div>Currently Available</div>
+              <div>Net Worth</div>
+              <div>Searches Owned</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+            <div className={classes.Row}>
+              <div>1</div>
+              <div>person@</div>
+              <div>t$482910</div>
+              <div>t$5921049</div>
+              <div>428</div>
+            </div>
+          </div>
+        </div>
+        <div className={classes.RankingChartContainer}>chart here</div>
       </div>
       <div className={classes.CompInfoContainer}>
-        <div className={classes.UserRankContainer}></div>
-        <div className={classes.CompDetailsContainer}></div>
+        <div className={classes.UserRankContainer}>Ranking here</div>
+        <div className={classes.CompDetailsContainer}>Comp status here</div>
       </div>
     </div>
   )
