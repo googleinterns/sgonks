c526712b15575dc5f2817dc873494b425851ad2d
lin8231 <lin8231@outlook.com>
2021-01-06 15:48:48 +1300

    Manual linting fix

--- portfolio/frontend/src/App.js
+++ portfolio/frontend/src/App.js
@@ -1,11 +1,11 @@
-import "./App.css"
-import { BrowserRouter } from "react-router-dom"
-import SGonksPlatfrom from "./containers/SGonksPlatform/SGonksPlatform"
-
+import './App.css'
+import {BrowserRouter} from 'react-router-dom'
+import SGonksPlatfrom from './containers/SGonksPlatform/SGonksPlatform'
+ 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
+      <div className='App'>
         <SGonksPlatfrom></SGonksPlatfrom>
       </div>
     </BrowserRouter>
