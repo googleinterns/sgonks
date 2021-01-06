34d0eca0a8c8b3390b006a186903586423a7deb2
lin8231 <lin8231@outlook.com>
2021-01-07 09:37:50 +1300

    Add top half of page in mysgonks prototype

--- portfolio/frontend/src/containers/SGonksPlatform/MySGonks/MySGonks.js
+++ portfolio/frontend/src/containers/SGonksPlatform/MySGonks/MySGonks.js
@@ -1,9 +1,28 @@
 import React from "react"
 import classes from "./MySGonks.module.css"
+import placeholderChart from "../../../assets/placeholderChart.png"
+import Marketplace from "../Marketplace/Marketplace"
 
 const MySGonks = (props) => {
   return (
-    <div>
+    <div className={classes.MySGonks}>
+      <div className={classes.ChartAndInfoContainer}>
+        <div className={classes.ChartContainer}>
+          <img src={placeholderChart}></img>
+        </div>
+        <div className={classes.InfoContainer}>
+          <div className={classes.NameInfoContainer}>
+            <p><strong>Firstname Lastname</strong></p>
+            <p>Initial Worth: t$100</p>
+            <p>Net worth: t$19492</p>
+          </div>
+          <div className={classes.currentlyAvailableContainer}>
+            <p><strong>Currently available:</strong></p>
+            <p>t$4918</p>
+          </div>
+          <Link to={Marketplace}>Buy sGonks</Link>
+        </div>
+      </div>
       
     </div>
   )
