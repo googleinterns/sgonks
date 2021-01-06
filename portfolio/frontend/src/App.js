f4cea9b2e049d9d20807cd939c40ad3901599097
roonil-wazlib <emma.hogan314@gmail.com>
2021-01-07 12:40:52 +1300

    Update app to go to login page when site launched

--- portfolio/frontend/src/App.js
+++ portfolio/frontend/src/App.js
@@ -1,12 +1,14 @@
 import './App.css'
 import {BrowserRouter} from 'react-router-dom'
 import SGonksPlatfrom from './containers/SGonksPlatform/SGonksPlatform'
+import Login from './containers/Login/Login'
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-        <SGonksPlatfrom></SGonksPlatfrom>
+        {/* <SGonksPlatfrom></SGonksPlatfrom> */}
+        <Login></Login>
       </div>
     </BrowserRouter>
   )
