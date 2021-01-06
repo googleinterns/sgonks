e81635789a615b6beedf3f05dffcf2f9fd92f4e4
lin8231 <lin8231@outlook.com>
2021-01-06 14:37:16 +1300

    Add styling to larger components

--- portfolio/frontend/src/containers/SGonksPlatform/Competition/Competition.js
+++ portfolio/frontend/src/containers/SGonksPlatform/Competition/Competition.js
@@ -3,8 +3,15 @@ import classes from "./Competition.module.css"
 
 const Competition = (props) => {
   return (
-    <div>
-      <p>This is the Competition Page.</p>
+    <div className={classes.Competition}>
+      <div className={classes.RankingInfoContainer}>
+        <div className={classes.RankingsContainer}></div>
+        <div className={classes.RankingChartContainer}></div>
+      </div>
+      <div className={classes.CompInfoContainer}>
+        <div className={classes.UserRankContainer}></div>
+        <div className={classes.CompDetailsContainer}></div>
+      </div>
     </div>
   )
 }
