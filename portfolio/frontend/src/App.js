fa056664b56d227cabf4bb91cb08b97162fe3112
roonil-wazlib <emma.hogan314@gmail.com>
2021-01-07 17:16:05 +1300

    Styling fixes

--- portfolio/frontend/src/App.js
+++ portfolio/frontend/src/App.js
@@ -1,22 +1,18 @@
 import './App.css'
-import {BrowserRouter} from 'react-router-dom'
+
 import {useState} from 'react';
+import {BrowserRouter} from 'react-router-dom'
 
 import Login from './containers/Login/Login'
 import SGonksPlatfrom from './containers/SGonksPlatform/SGonksPlatform'
 
 function App() {
+  const [content, setContent] = useState(<Login handler={
+    () => setContent(<SGonksPlatfrom></SGonksPlatfrom>)}></Login>)
 
-  const [content, setContent] = useState(<Login handler={() => setContent(<SGonksPlatfrom></SGonksPlatfrom>)}></Login>)
-
-  return (
-    <BrowserRouter>
-      <div className='App'>
-        {content}
-      </div>
-    </BrowserRouter>
-  )
+    return (<BrowserRouter>
+            <div className = 'App'>{content}</div>
+    </BrowserRouter>)
 }
 
 export default App
-
