c317774e714a0c509b338b551b4a81fae7665e82
lin8231 <lin8231@outlook.com>
2021-01-08 11:05:59 +1300

    Change double quotes to single quotes to satisfy linter

--- portfolio/frontend/src/App.js
+++ portfolio/frontend/src/App.js
@@ -1,15 +1,15 @@
-import "./App.css"
-import { BrowserRouter, Route } from "react-router-dom"
+import './App.css'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import Login from "./containers/Login/Login"
-import SGonksPlatfrom from "./containers/SGonksPlatform/SGonksPlatform"
+import Login from './containers/Login/Login'
+import SGonksPlatfrom from './containers/SGonksPlatform/SGonksPlatform'
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Route path="/" exact component={Login}></Route>
-        <Route path="/sgonks-platform" component={SGonksPlatfrom}></Route>
+      <div className='App'>
+        <Route path='/' exact component={Login}></Route>
+        <Route path='/sgonks-platform' component={SGonksPlatfrom}></Route>
       </div>
     </BrowserRouter>
   )
