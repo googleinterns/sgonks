8a9ac50ed54b9519e64e7482995617626ce3ef94
roonil-wazlib <emma.hogan314@gmail.com>
2021-01-12 17:24:56 +1300

    Rework file structure, attempt to link backend with react

---
+++ project/src/main/java/com/google/sps/servlets/ReactServlet.java
@@ -0,0 +1,29 @@
+
+package com.google.sps.servlets;
+
+import com.google.sps.servlets.ServletUtils;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import javax.servlet.ServletException;
+
+@WebServlet(
+    urlPatterns = {
+      "/test",
+      "/anotherTest"
+    })
+public class ReactServlet extends HttpServlet {
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException, ServletException {
+    String servletPath = request.getServletPath();
+    if (servletPath.equals("/test")) {
+      System.out.println("GOING");
+      request.setAttribute("TITLE", "test");
+      ServletUtils.RenderReact("App", request, response);
+    }
+    request.setAttribute("TITLE", "test");
+    ServletUtils.RenderReact("App", request, response);
+  }
+}
