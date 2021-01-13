dc352189abd4cd0ed847f91d4d56f6e670464fcf
lin8231 <lin8231@outlook.com>
2021-01-14 11:04:42 +1300

    Remove duplicate browser router import

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -6,7 +6,6 @@ import {BrowserRouter} from "react-router-dom";
 
 import App from "./App"
 import reportWebVitals from "./reportWebVitals"
-import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
   <BrowserRouter>
