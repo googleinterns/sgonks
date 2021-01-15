378b24993dd4f6ce02661c8b46bf6af4fc84d6ed
Phoebe Khokgawe <khokgawep@gmail.com>
2021-01-15 18:13:56 +1300

    CompetitorInfo is now an AutoValue class and can generates it's own code.

--- project/src/main/java/com/google/sps/data/CompetitorInfo.java
+++ project/src/main/java/com/google/sps/data/CompetitorInfo.java
@@ -13,36 +13,26 @@
 // limitations under the License.
 
 package com.google.sps.data;
-
+import com.google.auto.value.AutoValue;
 /**
  * Class representing user data specific to a competition
  */
-public class CompetitorInfo {
+@AutoValue
+public abstract class CompetitorInfo {
+    public static CompetitorInfo create(int rank, int rankYesterday, String name, String idap, long networth, long amtAvailable) {
+        return new AutoValue_CompetitorInfo( rank,  rankYesterday,  name,  idap,  networth,  amtAvailable);
+    }
 
     /** The current ranking of the user in this competition based on networth */
-    private int rank;
-
+    public abstract int rank();
     /** The rank of the user yesterday for purposes of displaying rises/falls in leaderboard */
-    private int rankYesterday;
-
+    public abstract int rankYesterday();
     /** The competitor's name */
-    private String name;
-
+    public abstract String name();
     /** The competitor's idap */
-    private String idap;
-
+    public abstract String idap();
     /** The networth of the competitor */
-    private long networth;
-
+    public abstract long networth();
     /** The amount the user has available for additional investments */
-    private long amtAvailable;
-
-    public CompetitorInfo(int rank, int rankYesterday, String name, String idap, long networth, long amtAvailable) {
-        this.rank = rank;
-        this.rankYesterday = rankYesterday;
-        this.name = name;
-        this.idap = idap;
-        this.networth = networth;
-        this.amtAvailable = amtAvailable;
-    }
+    public abstract long amtAvailable();
 }
--- project/src/main/java/com/google/sps/servlets/CompetitionInfoServlet.java
+++ project/src/main/java/com/google/sps/servlets/CompetitionInfoServlet.java
@@ -40,15 +40,15 @@ public class CompetitionInfoServlet extends HttpServlet {
 
   private void getUserCompetitions() {
     List<CompetitorInfo> competitors = new ArrayList<>();
-    competitors.add(new CompetitorInfo(1, 1, "Bob", "bobk@", 1000, 500));
-    competitors.add(new CompetitorInfo(2, 2, "Jack", "jackm@", 800, 450));
+    competitors.add(CompetitorInfo.create(1, 1, "Bob", "bobk@", 1000, 500));
+    competitors.add(CompetitorInfo.create(2, 2, "Jack", "jackm@", 800, 450));
     Competition compObj = new Competition(1895, "Google-Clouds-Comp", "Emma Hogan", "emmahogan@",
         new Date(2021, 1, 1).getTime(), new Date(2021, 1, 10).getTime(), competitors);
 
     List<CompetitorInfo> competitors2 = new ArrayList<>();
-    competitors2.add(new CompetitorInfo(1, 1, "Mary", "maryk@", 1000, 500));
-    competitors2.add(new CompetitorInfo(2, 2, "Bell", "bellm@", 800, 450));
-    competitors2.add(new CompetitorInfo(3, 3, "Bob", "bobk@", 700, 400));
+    competitors2.add( CompetitorInfo.create(1, 1, "Mary", "maryk@", 1000, 500));
+    competitors2.add( CompetitorInfo.create(2, 2, "Bell", "bellm@", 800, 450));
+    competitors2.add( CompetitorInfo.create(3, 3, "Bob", "bobk@", 700, 400));
     Competition compObj2 = new Competition(1896, "Tide Pod", "Mercury Lin", "mercurylin@",
         new Date(2021, 1, 1).getTime(), new Date(2021, 1, 10).getTime(), competitors2);
 
