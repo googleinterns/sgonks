548937b91eaada799c0bace47684eab81a7df300
lin8231 <lin8231@outlook.com>
2021-01-19 10:17:48 +1300

    Fetching competition data works fine

--- project/frontend/src/containers/SelectCompetition/SelectCompetition.js
+++ project/frontend/src/containers/SelectCompetition/SelectCompetition.js
@@ -1,8 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./SelectCompetition.module.css";
 import { Link } from "react-router-dom";
 
 const SelectCompetition = (props) => {
+  useEffect(() => {
+    fetch("./competitionInfo?compId=" + "123")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+  });
+
   return (
     <div className={classes.SelectCompContainer}>
       <p>select comp page</p>
