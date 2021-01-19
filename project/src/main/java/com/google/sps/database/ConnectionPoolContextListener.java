c9cd11c8c669e7a162a8a5ed0461d42e07b0f227
roonil-wazlib <emma.hogan314@gmail.com>
2021-01-20 10:57:27 +1300

    Add first attempt at db setup

---
+++ project/src/main/java/com/google/sps/database/ConnectionPoolContextListener.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.sps.database;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+import javax.sql.DataSource;
+
+@SuppressFBWarnings(
+    value = {"HARD_CODE_PASSWORD", "WEM_WEAK_EXCEPTION_MESSAGING"},
+    justification = "Extracted from environment, Exception message adds context.")
+@WebListener("Creates a connection pool that is stored in the Servlet's context for later use.")
+public class ConnectionPoolContextListener implements ServletContextListener {
+
+  // Saving credentials in environment variables is convenient, but not secure - consider a more
+  // secure solution such as https://cloud.google.com/kms/ to help keep secrets safe.
+  private static final String CLOUD_SQL_CONNECTION_NAME =
+      System.getenv("CLOUD_SQL_CONNECTION_NAME");
+
+  @SuppressFBWarnings(
+      value = "USBR_UNNECESSARY_STORE_BEFORE_RETURN",
+      justification = "Necessary for sample region tag.")
+  private DataSource createConnectionPool() {
+    // [START cloud_sql_mysql_servlet_create]
+    // The configuration object specifies behaviors for the connection pool.
+    HikariConfig config = new HikariConfig();
+
+    config.setJdbcUrl(String.format("jdbc:mysql://localhost:3306/%s", "my-instance"));
+    config.setUsername("root"); // e.g. "root", "mysql"
+    config.setPassword("Sgonks!Sgonks!Sgonks!"); // e.g. "my-password"
+
+    config.addDataSourceProperty("socketFactory", "com.google.cloud.sql.mysql.SocketFactory");
+    config.addDataSourceProperty("cloudSqlInstance", CLOUD_SQL_CONNECTION_NAME);
+    config.addDataSourceProperty("ipTypes", "PUBLIC,PRIVATE");
+
+    config.setMaximumPoolSize(5);
+    // minimumIdle is the minimum number of idle connections Hikari maintains in the pool.
+    // Additional connections will be established to meet this value unless the pool is full.
+    config.setMinimumIdle(5);
+
+    // setConnectionTimeout is the maximum number of milliseconds to wait for a connection checkout.
+    // Any attempt to retrieve a connection from this pool that exceeds the set limit will throw an
+    // SQLException.
+    config.setConnectionTimeout(10000); // 10 seconds
+    // idleTimeout is the maximum amount of time a connection can sit in the pool. Connections that
+    // sit idle for this many milliseconds are retried if minimumIdle is exceeded.
+    config.setIdleTimeout(600000); // 10 minutes
+    config.setMaxLifetime(1800000); // 30 minutes
+
+    // Initialize the connection pool using the configuration object.
+    return new HikariDataSource(config);
+  }
+
+  private void createTable(DataSource pool) throws SQLException {
+    // Safely attempt to create the table schema.
+    try (Connection conn = pool.getConnection()) {
+      String stmt =
+          "CREATE TABLE IF NOT EXISTS users ( "
+              + "vote_id SERIAL NOT NULL, time_cast timestamp NOT NULL, candidate CHAR(6) NOT NULL,"
+              + " PRIMARY KEY (vote_id) );";
+      try (PreparedStatement createTableStatement = conn.prepareStatement(stmt);) {
+        createTableStatement.execute();
+      }
+    }
+  }
+
+  @Override
+  public void contextDestroyed(ServletContextEvent event) {
+    // This function is called when the Servlet is destroyed.
+    HikariDataSource pool = (HikariDataSource) event.getServletContext().getAttribute("my-pool");
+    if (pool != null) {
+      pool.close();
+    }
+  }
+
+  @Override
+  public void contextInitialized(ServletContextEvent event) {
+    // This function is called when the application starts and will safely create a connection pool
+    // that can be used to connect to.
+    ServletContext servletContext = event.getServletContext();
+    DataSource pool = (DataSource) servletContext.getAttribute("my-pool");
+    if (pool == null) {
+      pool = createConnectionPool();
+      servletContext.setAttribute("my-pool", pool);
+    }
+    try {
+      createTable(pool);
+    } catch (SQLException ex) {
+      throw new RuntimeException(
+          "Unable to verify table schema. Please double check the steps"
+              + "in the README and try again.",
+          ex);
+    }
+  }
+}
