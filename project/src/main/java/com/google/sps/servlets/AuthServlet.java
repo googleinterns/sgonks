53e8ce89fafad81eda7405672dd949b89e768567
roonil-wazlib <emma.hogan314@gmail.com>
2021-01-21 13:47:40 +1300

    Update auth servlet to query db and respond to user email

--- project/src/main/java/com/google/sps/servlets/AuthServlet.java
+++ project/src/main/java/com/google/sps/servlets/AuthServlet.java
@@ -22,17 +22,55 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.sps.data.*;  
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 @WebServlet("/authentication")
 public class AuthServlet extends HttpServlet {
 
+  private static final Logger LOGGER = Logger.getLogger(AuthServlet.class.getName());
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    User userObj = User.create(1896,"Phoebe Khokgawe","phoebek@");
+    DataSource pool = (DataSource) request.getServletContext().getAttribute("my-pool");
 
-    Gson gson = new Gson();
-    
-    response.setContentType("application/json");
+    try (Connection conn = pool.getConnection()) {
+      String email = request.getParameter("email");
+      User user = getUser(conn, email);
 
-    response.getWriter().println(gson.toJson(userObj));
+      Gson gson = new Gson();
+      response.setContentType("application/json");
+      response.getWriter().println(gson.toJson(user));
+      //response.setContentType("text/html;");
+      //response.getWriter().println("<h1>" + user + "</h1>");
+    } catch (SQLException ex) {
+      LOGGER.log(Level.WARNING, "Error while attempting to find user.", ex);
+      response.setStatus(500);
+      response.getWriter().write("Unable to successfully fetch user");
+    }
+  }
+
+  private User getUser(Connection conn, String email) throws SQLException {
+    String stmt = "SELECT id, name FROM users WHERE email='" + email + "';";
+    try (PreparedStatement userStmt = conn.prepareStatement(stmt);) {
+      // Execute the statement
+      ResultSet userRs = userStmt.executeQuery();
+      // Convert a result into User object
+      int id;
+      String name;
+      User user;
+      while (userRs.next()) {
+        id = userRs.getInt(1);
+        name = userRs.getString(2);
+        return User.create(id, name, email);
+      }
+      return null;
+    }
   }
 }
