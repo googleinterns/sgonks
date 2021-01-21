d7c4c262f387f0e20c8afcfb9b146b00235106bc
lin8231 <lin8231@outlook.com>
2021-01-22 10:07:46 +1300

    Write tests for HeaderBar component

--- project/frontend/src/components/HeaderBar/HeaderBar.test.js
+++ project/frontend/src/components/HeaderBar/HeaderBar.test.js
@@ -4,12 +4,29 @@ import Adapter from "enzyme-adapter-react-16";
 
 import HeaderBar from "./HeaderBar";
 import LoginButtonSet from "../UI/LoginButtonSet/LoginButtonSet";
+import SignOutButton from "./SignOutButton/SignOutButton";
+import { NavLink } from "react-router-dom";
 
 configure({ adapter: new Adapter() });
 
 describe("<HeaderBar />", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<HeaderBar />);
+  });
+
   it('should render "What is sGonks?" and "Sign in" buttins if not authenticated', () => {
-    const wrapper = shallow(<HeaderBar />);
-    expect(wrapper.find(LoginButtonSet)).toHaveLength(1);
+    expect(wrapper.find(LoginButtonSet));
+  });
+
+  it('should render "Select competition" and "Sign out" buttins if authenticated', () => {
+    wrapper.setProps({ loggedIn: true });
+    expect(wrapper.find(SignOutButton));
+    expect(wrapper.find(NavLink).text()).toEqual("Select Competition");
+  });
+
+  it('should render inner navigation items if "innerNav" is true', () => {
+    wrapper.setProps({ innerNav: true });
+    expect(wrapper.find(NavLink)).toHaveLength(3);
   });
 });
