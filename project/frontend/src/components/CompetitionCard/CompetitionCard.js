401369f0f28cfa269497c37823b3dfa3f58b8338
lin8231 <lin8231@outlook.com>
2021-01-21 13:29:16 +1300

    Implement basic elements of competition card

--- project/frontend/src/components/CompetitionCard/CompetitionCard.js
+++ project/frontend/src/components/CompetitionCard/CompetitionCard.js
@@ -1,10 +1,30 @@
 import React, { useEffect } from "react";
 import classes from "./CompetitionCard.module.css";
+import LinkButton from "../UI/LinkButton/LinkButton";
 
 const CompetitionCard = (props) => {
   return (
     <li className={classes.CompetitionCardContainer}>
-      <p>cdsaard</p>
+      <div className={classes.CompetitionInfoContainer}>
+        <div className={classes.CompetitionIdentificationContainer}>
+          <h1 className={classes.CompetitionTitle}>Title</h1>
+          <h2>Organised by</h2>
+        </div>
+        <div className={classes.CompetitionDetailsContainer}>
+          <h3>Competition Details</h3>
+          <p>Start date</p>
+          <p>End date</p>
+        </div>
+      </div>
+      <div className={classes.CompetitionPersonalInfoContainer}>
+        <p>Ranking</p>
+        <div className={classes.UserCompInfo}>
+          <p>Initial worth</p>
+          <p>Current worth</p>
+          <p>Currently available</p>
+        </div>
+        <LinkButton inverted>View Competition</LinkButton>
+      </div>
     </li>
   );
 };
