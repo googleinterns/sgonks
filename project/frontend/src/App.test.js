1b63a2520d73e7f441e32ccbc5aa9da7d5c3c1f0
lin8231 <lin8231@outlook.com>
2021-01-22 11:53:41 +1300

    Add tests for LandingPage

--- project/frontend/src/App.test.js
+++ project/frontend/src/App.test.js
@@ -5,12 +5,41 @@ import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 import HeaderBar from "./components/HeaderBar/HeaderBar";
+import { Route } from "react-router-dom";
+import SelectCompetition from "./containers/SelectCompetition/SelectCompetition";
 
 configure({ adapter: new Adapter() });
 
 describe("<App />", () => {
-  it("should render one HeaderBar element", () => {
-    const wrapper = shallow(<App />);
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<App />);
+  });
+
+  it("should render", () => {
+    expect(wrapper.exists()).toBeTruthy();
+  });
+
+  it("should always render one HeaderBar element", () => {
     expect(wrapper.find(HeaderBar)).toHaveLength(1);
   });
+
+  // it("should enable routing to SelectCompetition if user is logged in", () => {
+  //   // Cache original functionality
+  //   const realUseState = React.useState;
+
+  //   // Stub the initial state
+  //   const stubInitialState = { signedIn: true };
+
+  //   // Mock useState before rendering your component
+  //   jest
+  //     .spyOn(React, "useState")
+  //     .mockImplementationOnce(() => realUseState(stubInitialState));
+
+  //   expect(
+  //     wrapper.contains(
+  //       <Route path="/compselect" component={SelectCompetition}></Route>
+  //     )
+  //   ).toBeTruthy();
+  // });
 });
--- project/frontend/src/containers/LandingPage/LandingPage.js
+++ project/frontend/src/containers/LandingPage/LandingPage.js
@@ -1,20 +1,21 @@
-import React from 'react';
+import React from "react";
 import classes from "./LandingPage.module.css";
-import LoginButtonSet from "../../components/UI/LoginButtonSet/LoginButtonSet"
+import LoginButtonSet from "../../components/UI/LoginButtonSet/LoginButtonSet";
 
-const LandingPage = props => {
-    return(
-        <div className={classes.LandingPageContent}>
-            <div className={classes.Hero}>
-                <h1>Who's the next trendsetter?</h1>
-                <h3>Something something a description of what the game does, what benefits it has. Something something ...</h3>
-                <LoginButtonSet></LoginButtonSet>
-            </div>
-            <div className={classes.IllustrationContainer}>
-
-            </div>
-        </div>
-    )
-}
+const LandingPage = (props) => {
+  return (
+    <div className={classes.LandingPageContent}>
+      <div className={classes.Hero}>
+        <h1>Who's the next trendsetter?</h1>
+        <h3>
+          Something something a description of what the game does, what benefits
+          it has. Something something ...
+        </h3>
+        <LoginButtonSet></LoginButtonSet>
+      </div>
+      <div className={classes.IllustrationContainer}></div>
+    </div>
+  );
+};
 
 export default LandingPage;
