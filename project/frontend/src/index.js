a531fc81e7e5edc40ed3c5ab6e25732e18e2e372
lin8231 <lin8231@outlook.com>
2021-01-25 11:21:19 +1300

    Delete unnecessary import

--- project/frontend/src/index.js
+++ project/frontend/src/index.js
@@ -2,7 +2,6 @@ import "./index.css";
 
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
