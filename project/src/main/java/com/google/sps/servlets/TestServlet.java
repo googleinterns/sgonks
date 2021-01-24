67f496966db7ecf6b75b9f3314f55d7ae3363027
roonil-wazlib <emma.hogan314@gmail.com>
2021-01-25 12:34:21 +1300

    Add test of datastore data retrieval in java

--- project/src/main/java/com/google/sps/servlets/TestServlet.java
+++ project/src/main/java/com/google/sps/servlets/TestServlet.java
@@ -37,10 +37,8 @@ import javax.sql.DataSource;
 import com.google.sps.data.*;
 
 // Imports the Google Cloud client library
-import com.google.cloud.datastore.Datastore;
-import com.google.cloud.datastore.DatastoreOptions;
-import com.google.cloud.datastore.Entity;
-import com.google.cloud.datastore.Key;
+import com.google.cloud.datastore.*;
+
 
 
 
@@ -83,7 +81,7 @@ public class TestServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-    testDatastore();
+    Entity trend = testDatastore();
 
     String name = request.getParameter("name");
     String email = request.getParameter("email");
@@ -119,32 +117,21 @@ public class TestServlet extends HttpServlet {
     response.setContentType("text/html;");
     response.getWriter().println("<p>going</p>");
     response.getWriter().println("<h1>" + users + "</h1>");
+    response.getWriter().println("<h1>" + trend + "</h1>");
   }
 
-  private void testDatastore() {
+  private Entity testDatastore() {
     // Instantiates a client
     Datastore datastore = DatastoreOptions.getDefaultInstance().getService();
 
-    // The kind for the new entity
-    String kind = "Task";
-    // The name/ID for the new entity
-    String name = "example";
-    // The Cloud Datastore key for the new entity
-    Key taskKey = datastore.newKeyFactory().setKind(kind).newKey(name);
+    //fetch test data to confirm working
+    Query<Entity> query = Query.newEntityQueryBuilder().setKind("TrendsData").build();
+    QueryResults<Entity> trends = datastore.run(query);
+    Entity trend;
 
-    // Prepares the new entity
-    Entity task = Entity.newBuilder(taskKey)
-        .set("description", "Buy milk")
-        .build();
-
-    // Saves the entity
-    datastore.put(task);
-
-    System.out.printf("Saved %s: %s%n", task.getKey().getName(), task.getString("description"));
-
-    //Retrieve entity
-    Entity retrieved = datastore.get(taskKey);
-
-    System.out.printf("Retrieved %s: %s%n", taskKey.getName(), retrieved.getString("description"));
+    while (trends.hasNext()) {
+      return trends.next();
+    }
+    return null;
   }
 }
