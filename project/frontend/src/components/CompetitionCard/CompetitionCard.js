4fc2b0e79be8532ed8e03edde21881a91455ec5e
lin8231 <lin8231@outlook.com>
2021-01-25 13:04:14 +1300

    Change CompetitionCards to render info based on props received

--- project/frontend/src/components/CompetitionCard/CompetitionCard.js
+++ project/frontend/src/components/CompetitionCard/CompetitionCard.js
@@ -3,19 +3,19 @@ import classes from "./CompetitionCard.module.css";
 import LinkButton from "../UI/LinkButton/LinkButton";
 
 const CompetitionCard = (props) => {
-  console.log("props");
-  console.log(props);
   return (
     <li className={classes.CompetitionCardContainer}>
       <div className={classes.CompetitionInfoContainer}>
         <div className={classes.CompIdentification}>
-          <h1 className={classes.CompetitionTitle}>Title</h1>
-          <h2 className={classes.InfoText}>Organised by</h2>
+          <h1 className={classes.CompetitionTitle}>{props.name}</h1>
+          <h2 className={classes.InfoText}>
+            Organised by {props.organiser} ({props.organiserLdap})
+          </h2>
         </div>
         <div className={classes.CompetitionDetails}>
           <h3>Competition Details</h3>
-          <p>Start date: </p>
-          <p>End date:</p>
+          <p>Start date: &nbsp; {new Date(props.startDate).toLocaleString()}</p>
+          <p>End date: &nbsp; {new Date(props.endDate).toLocaleString()}</p>
         </div>
       </div>
       <div className={classes.CompetitionPersonalInfoContainer}>
@@ -23,9 +23,9 @@ const CompetitionCard = (props) => {
           Current ranking: <span>0th</span>
         </p>
         <div className={classes.UserCompInfo}>
-          <p>Initial worth:</p>
-          <p>Current net worth:</p>
-          <p>Currently available:</p>
+          <p>Initial worth: ?????</p>
+          <p>Current net worth: ?????</p>
+          <p>Currently available: ?????</p>
         </div>
         <LinkButton inverted="true">View Competition</LinkButton>
       </div>
