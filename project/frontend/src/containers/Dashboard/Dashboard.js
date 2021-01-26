7aaf2be091be878414c93035c8e962b7882affc7
lin8231 <lin8231@outlook.com>
2021-01-27 11:10:37 +1300

    Add general html for dashboard

--- project/frontend/src/containers/Dashboard/Dashboard.js
+++ project/frontend/src/containers/Dashboard/Dashboard.js
@@ -1,10 +1,40 @@
 import React from "react";
 import classes from "./Dashboard.module.css";
+import classnames from "classnames";
+import LinkButton from "../../components/UI/LinkButton/LinkButton";
 
 const Dashboard = (props) => {
   return (
     <div className={classes.DashboardContainer}>
-      <p>dashboard</p>
+      <div className={classes.Column}>
+        <p>
+          Welcome back, <span>Firstname</span>!
+        </p>
+        <div className={classnames(classes.Block, classes.CompInfo)}>
+          compinfo
+        </div>
+        <div className={classnames(classes.Block, classes.TeammateBuy)}>
+          xx bought
+        </div>
+      </div>
+      <div className={classes.Column}>
+        <div className={classnames(classes.Block, classes.YourSGonks)}>
+          your sgonks
+        </div>
+        <div className={classnames(classes.Block, classes.ChartContainer)}>
+          chart here
+        </div>
+        <LinkButton inverted="true">View my sGonks</LinkButton>
+      </div>
+      <div className={classes.Column}>
+        <div className={classnames(classes.Block, classes.moneyInfo)}>
+          money info
+        </div>
+        <LinkButton>Buy sGonks</LinkButton>
+        <div className={classnames(classes.TrendingSearches)}>
+          trending searches
+        </div>
+      </div>
     </div>
   );
 };
