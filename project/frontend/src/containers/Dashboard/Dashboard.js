f1aa63ce09958a12d204c08deb1069eb72492138
lin8231 <lin8231@outlook.com>
2021-01-26 17:13:42 +1300

    Set up dashboard files and change app routing to point to this

---
+++ project/frontend/src/containers/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import classes from "./Dashboard.module.css";
+
+const Dashboard = (props) => {
+  return (
+    <div className={classes.DashboardContainer}>
+      <p>dashboard</p>
+    </div>
+  );
+};
+
+export default Dashboard;
