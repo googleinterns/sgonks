6d9d4a1fc10ca05e212c2f2f40ac331e15bcd6a8
lin8231 <lin8231@outlook.com>
2021-01-27 13:48:17 +1300

    Refactor Block to take classnames instead

--- project/frontend/src/components/UI/Block/Block.js
+++ project/frontend/src/components/UI/Block/Block.js
@@ -1,9 +1,13 @@
 import React from "react";
 import classes from "./Block.module.css";
+import classnames from "classnames";
 
 const Block = (props) => {
   return (
-    <div className={classes.Block} style={props.style}>
+    <div
+      className={classnames(classes.Block, props.className)}
+      style={props.style}
+    >
       <div className={classes.ContentWrapper}>{props.children}</div>
     </div>
   );
--- project/frontend/src/containers/Dashboard/Dashboard.js
+++ project/frontend/src/containers/Dashboard/Dashboard.js
@@ -13,23 +13,23 @@ const Dashboard = (props) => {
             Welcome back, <span>Firstname</span>!
           </h1>
         </div>
-        <Block style={{ height: "44%" }}>
+        <Block className={classes.CompInfo}>
           <h2>Time until end of competition:</h2>
           <p>20 days 19 hours etc</p>
           <h2>Your current ranking</h2>
           <p>1nd</p>
         </Block>
-        <Block style={{ height: "41%" }}>xx bought</Block>
+        <Block className={classes.TeammateBuys}>xx bought</Block>
       </div>
       <div className={classes.Column}>
-        <Block style={{ height: "50%" }}>your sgonks</Block>
-        <Block style={{ height: "37%" }}>chart here</Block>
+        <Block className={classes.YourSGonks}>your sgonks</Block>
+        <Block className={classes.ChartContainer}>chart here</Block>
         <LinkButton inverted="true">View my sGonks</LinkButton>
       </div>
       <div className={classes.Column}>
-        <Block style={{ height: "35%" }}>money info</Block>
+        <Block className={classes.MoneyInfo}>money info</Block>
         <LinkButton>Buy sGonks</LinkButton>
-        <Block style={{ height: "52%" }}>trending searches</Block>
+        <Block className={classes.TrendingSearches}>trending searches</Block>
       </div>
     </div>
   );
