a014e80227903e063d69adb44cdcbcf65eb88e62
lin8231 <lin8231@outlook.com>
2021-01-28 14:10:40 +1300

    delete unnecessary comments

--- project/frontend/src/App.test.js
+++ project/frontend/src/App.test.js
@@ -5,8 +5,6 @@ import Adapter from "enzyme-adapter-react-16";
 import App from "./App";
 
 import HeaderBar from "./components/HeaderBar/HeaderBar";
-import { Route } from "react-router-dom";
-import SelectCompetition from "./containers/SelectCompetition/SelectCompetition";
 
 configure({ adapter: new Adapter() });
 
@@ -23,23 +21,4 @@ describe("<App />", () => {
   it("should always render one HeaderBar element", () => {
     expect(wrapper.find(HeaderBar)).toHaveLength(1);
   });
-
-  // it("should enable routing to SelectCompetition if user is logged in", () => {
-  //   // Cache original functionality
-  //   const realUseState = React.useState;
-
-  //   // Stub the initial state
-  //   const stubInitialState = { signedIn: true };
-
-  //   // Mock useState before rendering your component
-  //   jest
-  //     .spyOn(React, "useState")
-  //     .mockImplementationOnce(() => realUseState(stubInitialState));
-
-  //   expect(
-  //     wrapper.contains(
-  //       <Route path="/compselect" component={SelectCompetition}></Route>
-  //     )
-  //   ).toBeTruthy();
-  // });
 });
