21ecc1201e1beb12c628fde05c501cc600ac8806
lin8231 <lin8231@outlook.com>
2021-01-28 16:36:43 +1300

    Populate elements with words

--- project/frontend/src/containers/Dashboard/Dashboard.js
+++ project/frontend/src/containers/Dashboard/Dashboard.js
@@ -15,21 +15,36 @@ const Dashboard = (props) => {
         </div>
         <Block className={classes.CompInfo}>
           <h2>Time until end of competition:</h2>
-          <p>20 days 19 hours etc</p>
-          <h2>Your current ranking</h2>
-          <p>1nd</p>
+          <p className={classes.CountDown}>20 days 19 hours etc</p>
+          <h2>Your current ranking:</h2>
+          <p className={classes.Ranking}>2nd</p>
         </Block>
-        <Block className={classes.TeammateBuys}>xx bought</Block>
+        <Block className={classes.TeammateBuys}>
+          xx bought... to be replaced with info display component
+        </Block>
       </div>
       <div className={classes.Column}>
-        <Block className={classes.YourSGonks}>your sgonks</Block>
+        <Block className={classes.YourSGonks}>
+          <h2>Your sGonks</h2>
+          <div className={classes.sGonksListContainer}>list container</div>
+        </Block>
         <Block className={classes.ChartContainer}>chart here</Block>
         <LinkButton inverted="true">View my sGonks</LinkButton>
       </div>
       <div className={classes.Column}>
-        <Block className={classes.MoneyInfo}>money info</Block>
+        <Block className={classes.MoneyInfo}>
+          <h2>Currently available:</h2>
+          <p>t$????</p>
+          <h2>Net worth:</h2>
+          <p>t$????</p>
+        </Block>
         <LinkButton>Buy sGonks</LinkButton>
-        <Block className={classes.TrendingSearches}>trending searches</Block>
+        <Block className={classes.TrendingSearches}>
+          <h2>Trending searches</h2>
+          <div className={classes.TrendingSearchListContainer}>
+            list container
+          </div>
+        </Block>
       </div>
     </div>
   );
