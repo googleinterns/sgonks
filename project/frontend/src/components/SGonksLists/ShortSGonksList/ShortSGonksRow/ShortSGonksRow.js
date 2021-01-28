4356f80c368da7a88ce142646a9ee081e0ae5f60
lin8231 <lin8231@outlook.com>
2021-01-28 21:00:45 +1300

    Change diff indicator to render dynamically based on data

--- project/frontend/src/components/SGonksLists/ShortSGonksList/ShortSGonksRow/ShortSGonksRow.js
+++ project/frontend/src/components/SGonksLists/ShortSGonksList/ShortSGonksRow/ShortSGonksRow.js
@@ -2,11 +2,31 @@ import React from "react";
 import classes from "./ShortSGonksRow.module.css";
 
 const ShortSGonksRow = (props) => {
+  const getDiffIndicator = () => {
+    const diff =
+      props.sgonk.currentPrice > props.sgonk.priceYesterday
+        ? "+"
+        : props.sgonk.currentPrice < props.sgonk.priceYesterday
+        ? "-"
+        : "=";
+
+    const style =
+      diff == "+"
+        ? { color: "green" }
+        : diff == "-"
+        ? { color: "red" }
+        : { color: "orange" };
+    return (
+      <div className={classes.ChangeIndicator} style={style}>
+        {diff}
+      </div>
+    );
+  };
   return (
     <div {...props} className={classes.Row}>
-      <div className={classes.ChangeArrow}>+</div>
+      {getDiffIndicator()}
       <div className={classes.SearchTerm}>{props.sgonk.searchTerm}</div>
-      <div className={classes.CurrentPrice}>$t{props.price}</div>
+      <div className={classes.CurrentPrice}>$t{props.sgonk.currentPrice}</div>
     </div>
   );
 };
