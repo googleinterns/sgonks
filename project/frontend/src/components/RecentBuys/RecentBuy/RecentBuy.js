654e72bb084cad8c87ecb4c80b7a978ad172e127
lin8231 <lin8231@outlook.com>
2021-01-29 14:00:12 +1300

    Refactor card backgroudn css

--- project/frontend/src/components/RecentBuys/RecentBuy/RecentBuy.js
+++ project/frontend/src/components/RecentBuys/RecentBuy/RecentBuy.js
@@ -4,17 +4,19 @@ import classes from "./RecentBuy.module.css";
 const RecentBuy = (props) => {
   return (
     <div className={classes.RecentBuy}>
-      <div className={classes.InvestorInfo}>
-        <p className={classes.Name}>Some Person</p>
-        <p className={classes.FillerWords}>invested</p>
-      </div>
-      <div className={classes.InvestmentInfo}>
-        <p className={classes.Amount}>t$3213</p>
-        <p className={classes.FillerWords}>in</p>
-        <p className={classes.Term}>"bitcoin"</p>
-      </div>
-      <div className={classes.TimeBought}>
-        <p>time</p>
+      <div className={classes.CardContainer}>
+        <div className={classes.InvestorInfo}>
+          <p className={classes.Name}>Some Person</p>
+          <p className={classes.FillerWords}>invested</p>
+        </div>
+        <div className={classes.InvestmentInfo}>
+          <p className={classes.Amount}>t$3213</p>
+          <p className={classes.FillerWords}>in</p>
+          <p className={classes.Term}>"bitcoin"</p>
+        </div>
+        <div className={classes.TimeBought}>
+          <p>time</p>
+        </div>
       </div>
     </div>
   );
