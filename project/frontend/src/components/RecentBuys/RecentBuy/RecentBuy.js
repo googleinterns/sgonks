0695c2150eae891be3eb68556ec5f4d1c88f1018
lin8231 <lin8231@outlook.com>
2021-01-29 13:19:03 +1300

    Create recent buy component

---
+++ project/frontend/src/components/RecentBuys/RecentBuy/RecentBuy.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./RecentBuy.module.css";
+
+const RecentBuy = (props) => {
+  return <div className={classes.RecentBuy}>buy</div>;
+};
+
+export default RecentBuy;
--- project/frontend/src/components/RecentBuys/RecentBuys.js
+++ project/frontend/src/components/RecentBuys/RecentBuys.js
@@ -2,13 +2,14 @@ import React from "react";
 import classes from "./RecentBuys.module.css";
 import { Slide } from "react-slideshow-image";
 import "react-slideshow-image/dist/styles.css";
+import RecentBuy from "./RecentBuy/RecentBuy";
 
 const RecentBuys = (props) => {
   return (
     <div className={classes.RecentBuysContainer}>
       <Slide easing="ease" className={classes.SlideContainer}>
         <div className={classes.EachSlide}>
-          <p>one slide</p>
+          <RecentBuy></RecentBuy>
         </div>
         <div className={classes.EachSlide}>
           <p>one slide</p>
