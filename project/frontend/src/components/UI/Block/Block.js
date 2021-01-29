92db9633941986e102ee9c5782c9d045ab786946
lin8231 <lin8231@outlook.com>
2021-01-29 15:39:04 +1300

    Add comment to Block

--- project/frontend/src/components/UI/Block/Block.js
+++ project/frontend/src/components/UI/Block/Block.js
@@ -2,6 +2,9 @@ import React from "react";
 import classes from "./Block.module.css";
 import classnames from "classnames";
 
+/* Content wrapper which wraps its children in a standardised background/border
+Can receive further styling from parent component on top of existing styling.
+*/
 const Block = (props) => {
   return (
     <div
