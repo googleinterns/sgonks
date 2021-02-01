0a0a3ea8fa3723045e4e78410acf7c58b0a792e6
lin8231 <lin8231@outlook.com>
2021-02-02 12:09:38 +1300

    Implement layout of text in info blocks

--- project/frontend/src/containers/MySGonks/MySGonks.js
+++ project/frontend/src/containers/MySGonks/MySGonks.js
@@ -7,11 +7,26 @@ const MySGonks = (props) => {
   return (
     <div className={classes.MySGonksContainer}>
       <div className={classes.ChartAndInfoContainer}>
-        <div className={classes.ChartContainer}>chart</div>
+        <Block className={classes.ChartContainer}>chart</Block>
         <div className={classes.InfoContainer}>
-          <Block className={classes.InfoBlock}>gen info</Block>
-          <Block className={classes.InfoBlock}>currently Avail</Block>
-          <LinkButton to="toroutelater">Buy sGonks</LinkButton>
+          <Block className={classes.InfoBlock}>
+            <h3>Firstname Lastname</h3>
+            <div className={classes.MonetaryInfo}>
+              <p>
+                <span>Initial worth:</span>
+                <span className={classes.TrendBucks}>t$</span>
+              </p>
+              <p>
+                <span>Net worth:</span>
+                <span className={classes.TrendBucks}>t$</span>
+              </p>
+            </div>
+          </Block>
+          <Block className={classes.InfoBlock}>
+            <h3>Currently Available:</h3>
+            <p className={classes.CurrentAvail}>t$</p>
+          </Block>
+          <LinkButton to="/toroutelater">Buy sGonks</LinkButton>
         </div>
       </div>
       <div className={classes.SGonksListContainer}>sgonks list</div>
