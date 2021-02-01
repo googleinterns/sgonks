8ee2c03d540cc9559c49d76cae821879564a1e4b
lin8231 <lin8231@outlook.com>
2021-02-02 11:50:04 +1300

    Implement layout of info sections

--- project/frontend/src/containers/MySGonks/MySGonks.js
+++ project/frontend/src/containers/MySGonks/MySGonks.js
@@ -1,12 +1,18 @@
 import React from "react";
 import classes from "./MySGonks.module.css";
+import Block from "../../components/UI/Block/Block";
+import LinkButton from "../../components/UI/LinkButton/LinkButton";
 
 const MySGonks = (props) => {
   return (
     <div className={classes.MySGonksContainer}>
       <div className={classes.ChartAndInfoContainer}>
         <div className={classes.ChartContainer}>chart</div>
-        <div className={classes.InfoContainer}>info</div>
+        <div className={classes.InfoContainer}>
+          <Block className={classes.InfoBlock}>gen info</Block>
+          <Block className={classes.InfoBlock}>currently Avail</Block>
+          <LinkButton to="toroutelater">Buy sGonks</LinkButton>
+        </div>
       </div>
       <div className={classes.SGonksListContainer}>sgonks list</div>
     </div>
