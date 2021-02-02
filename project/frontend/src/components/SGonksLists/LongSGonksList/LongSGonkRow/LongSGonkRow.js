35e662c92254fadf862b3518cf7536e2cdbc9fdb
lin8231 <lin8231@outlook.com>
2021-02-03 11:54:44 +1300

    Initialise files for sgonk rows

---
+++ project/frontend/src/components/SGonksLists/LongSGonksList/LongSGonkRow/LongSGonkRow.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./LongSGonkRow.module.css";
+
+const LongSGonkRow = (props) => {
+  return <div>row</div>;
+};
+
+export default LongSGonkRow;
---
+++ project/frontend/src/components/SGonksLists/LongSGonksList/LongSGonksList.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "./LongSGonksList.module.css";
+
+const LongSGonksList = (props) => {
+  return <div>list</div>;
+};
+
+export default LongSGonksList;
