600d7591faa2022618d0cb39f389abb125470bbf
roonil-wazlib <emma.hogan314@gmail.com>
2021-02-02 19:10:07 +1300

    Update script to run as cloud function

--- project/scripts/get_context_data.py
+++ project/scripts/get_context_data.py
@@ -14,24 +14,25 @@
 
 #!/usr/bin/env python3
 
-import sys
+import base64, json
 from google.cloud import datastore
 from fetch_trends import get_updated_daily_data
 from database_updates import update_investment_database
 
+def update(event, context):
+    eventdata = event["data"]
+    decoded = base64.b64decode(eventdata)
+    data = json.loads(decoded)
+    
+    start_date = int(data['date'])
+    google_search = data['search']
+    get_context(start_date, google_search)
 
-if __name__ == "__main__":
-    arguments = sys.argv
-    start_date = int(arguments[1])
-    google_search = arguments[2]
 
+def get_context(start_date, google_search):
     # Instantiates a client
     datastore_client = datastore.Client()
     # Retrieve up to date trends data for each search term
     daily_data = get_updated_daily_data(google_search, start_date)
     # Add up to date data do datastore
     update_investment_database(daily_data, datastore_client)
-
-
-
-
