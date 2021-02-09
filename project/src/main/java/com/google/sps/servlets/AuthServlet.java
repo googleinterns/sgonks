2c5455d962854b23f96fd942f233e31f86db9c5a
roonil-wazlib <emma.hogan314@gmail.com>
2021-02-10 12:15:41 +1300

    Remove unnecessary logs

--- project/src/main/java/com/google/sps/servlets/AuthServlet.java
+++ project/src/main/java/com/google/sps/servlets/AuthServlet.java
@@ -14,28 +14,26 @@
 
 package com.google.sps.servlets;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.auth.FirebaseToken;
-import com.google.gson.Gson;
-import com.google.sps.data.*;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
+import com.google.sps.data.*;  
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Connection;
+import java.sql.SQLException;
 import javax.sql.DataSource;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @WebServlet("/authentication")
 public class AuthServlet extends HttpServlet {
+
   private static final Logger LOGGER = Logger.getLogger(AuthServlet.class.getName());
 
   @Override
@@ -45,8 +43,6 @@ public class AuthServlet extends HttpServlet {
     try (Connection conn = pool.getConnection()) {
       String email = request.getParameter("email");
       String name = request.getParameter("name");
-      LOGGER.log(Level.WARNING, name);
-      LOGGER.log(Level.WARNING, email);
       User user = getUser(conn, email);
 
       if (user == null) {
@@ -57,53 +53,13 @@ public class AuthServlet extends HttpServlet {
       Gson gson = new Gson();
       response.setContentType("application/json");
       response.getWriter().println(gson.toJson(user));
-      // response.setContentType("text/html;");
-      // response.getWriter().println("<h1>" + user + "</h1>");
+      //response.setContentType("text/html;");
+      //response.getWriter().println("<h1>" + user + "</h1>");
     } catch (SQLException ex) {
       LOGGER.log(Level.WARNING, "Error while attempting to find user.", ex);
       response.setStatus(500);
       response.getWriter().write("Unable to successfully fetch user");
     }
-  }
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String uid = verifyIDToken(request);
-    //stored the user data in the session
-    request.getSession().setAttribute("uid",uid);
-  }
-
-  @Override
-  public void doDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    request.getSession().setAttribute("uid",null);
-  }
-
-  /**
-   *  Get the ID Token that is passed though request, verify the user and return the decoded uid.
-   * @param request -- request
-   * @return decoded ID Token.
-   * @throws IOException
-   */
-  private String verifyIDToken(HttpServletRequest request) throws IOException{
-    //get the client ID Token from the request body
-    StringBuilder buffer = new StringBuilder();
-    BufferedReader reader = request.getReader();
-    String line;
-    while ((line = reader.readLine()) != null) {
-      buffer.append(line);
-      buffer.append(System.lineSeparator());
-    }
-    String body = buffer.toString();
-
-    //decode and verify client ID token
-    FirebaseToken decodedToken = null;
-    try {
-      decodedToken = FirebaseAuth.getInstance().verifyIdToken(body);
-    } catch (FirebaseAuthException e) {
-      e.printStackTrace();
-    }
-
-    return decodedToken.getUid();
   }
 
   private User getUser(Connection conn, String email) throws SQLException {
@@ -125,7 +81,7 @@ public class AuthServlet extends HttpServlet {
   }
 
   private User addUser(Connection conn, String name, String email) throws SQLException {
-    String stmt = "INSERT INTO users (name, email) VALUES ('" + name + "', '" + email + "');";
+    String stmt = "INSERT INTO users (name, email) VALUES ('" + name + "'', '" + email + "'');";
     try (PreparedStatement userStmt = conn.prepareStatement(stmt);) {
       // Execute the statement
       userStmt.execute();
