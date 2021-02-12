c9643909b2fb80d9159811293dd7c5f0ab30dc22
Phoebe Khokgawe <khokgawep@gmail.com>
2021-02-12 19:02:47 +1300

    Implemented the logic for selling investments on mySGonks page.

--- project/frontend/src/components/SGonksLists/LongSGonksList/LongSGonkRow/LongSGonkRow.js
+++ project/frontend/src/components/SGonksLists/LongSGonksList/LongSGonkRow/LongSGonkRow.js
@@ -4,6 +4,22 @@ import classes from "./LongSGonkRow.module.css";
 const LongSGonkRow = (props) => {
   const priceDelta = props.currentValue - props.amountInvested;
 
+  const sellThisInvestment = async () => {
+    console.log("This function is called");
+    try {
+      await fetch("/sell", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: `id=${props.investmentId}`,
+      });
+      console.log("successfully sent the investment id: " + props.investmentId);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className={classes.Row}>
       <div className={classes.SearchQueryCell}>"{props.searchTerm}"</div>
@@ -19,7 +35,9 @@ const LongSGonkRow = (props) => {
         {priceDelta >= 0 ? "+" : "-"}t${Math.abs(priceDelta)}
       </div>
       <div className={classes.ButtonCell}>
-        <button className={classes.SellButton}>sell</button>
+        <button onClick={sellThisInvestment} className={classes.SellButton}>
+          sell
+        </button>
       </div>
     </div>
   );
