19ee7816293812729336b7fb66fcafd8a735c9c7
Phoebe Khokgawe <khokgawep@gmail.com>
2021-02-15 10:28:58 +1300

    When user login get the user ID from the database and saved it in the session for authenthication purposes.

--- project/src/main/java/com/google/sps/servlets/AuthServlet.java
+++ project/src/main/java/com/google/sps/servlets/AuthServlet.java
@@ -96,13 +96,10 @@ public class AuthServlet extends HttpServlet {
         response.getWriter().flush();
         return;
       }
-      /*
+
       String email = decodedToken.getEmail();
-
-      // get user from db using email
-
-      request.getSession().setAttribute("userID", userID)
-      */
+      long userID = getUserID(request,email);
+      request.getSession().setAttribute("userID", userID);
 
       response.setStatus(HttpServletResponse.SC_OK);
       response.flushBuffer();
@@ -121,6 +118,32 @@ public class AuthServlet extends HttpServlet {
     response.addCookie(emptyCookie);
     response.setStatus(HttpServletResponse.SC_OK);
     response.flushBuffer();
+  }
+
+  /**
+   * If user is in the database, get the user ID in the database from the given email.
+   * Otherwise add user to the database and return the user's ID.
+   * @param request -- HTTP request
+   * @param email -- user's email from the decoded token
+   * @return -- user's ID in the database
+   */
+  private long getUserID(HttpServletRequest request, String email){
+    DataSource pool = (DataSource) request.getServletContext().getAttribute("db-connection-pool");
+    long userID = -1;
+
+    try (Connection conn = pool.getConnection()) {
+      User user = getUser(conn,email);
+
+      if (user == null) {
+        // the user is not already in the database - add user to the database
+        //@TODO need to get the name from the frontend when it's implemented
+        user = addUser(conn, null, email);
+      }
+      userID = user.id();
+    } catch (SQLException ex) {
+      LOGGER.log(Level.WARNING, "Error while trying to get user's ID.", ex);
+    }
+    return userID;
   }
 
   private User getUser(Connection conn, String email) throws SQLException {
@@ -161,3 +184,4 @@ public class AuthServlet extends HttpServlet {
     }
   }
 }
+
